d83a57bd2c2867ba0697ecb6b0a07cd300d95524
Marco Melchiorri <marco.melchiorri@verisure.it>
2024-09-19 10:34:02 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const PORT = process.env.PORT_APP;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
