bb6ff85963501d724822f18f3e80c3b1785d06b9
Marco Melchiorri <marco.melchiorri@verisure.it>
2024-09-20 11:39:06 +0200

    added bodyParser.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT_APP;
 
