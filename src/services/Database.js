c7f6dbcdfd5b8b11351c1df44d8436ce1d66a714
Marco Melchiorri <marco.melchiorri@verisure.it>
2024-09-20 15:16:54 +0200

    created connection to database

--- src/services/Database.js
+++ src/services/Database.js
@@ -7,9 +7,29 @@ class Database {
     this.user = user;
     this.password = password;
     this.database = database;
+    this.connection = null; // To hold the DB connection
+  }
+
+  static instance = null; // Static property to hold the singleton instance
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database(
+        process.env.DB_HOST,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        process.env.DB_NAME
+      );
+    }
+    return Database.instance;
   }
 
   async connect() {
+    if (this.connection) {
+      console.log('Already connected to the database.');
+      return this.connection;
+    }
+
     console.log('Connecting to the database...');
     try {
       this.connection = await mysql.createConnection({
@@ -20,19 +40,13 @@ class Database {
       });
       console.log('Database connected');
     } catch (error) {
-      console.log(error);
+      console.error('Error connecting to the database:', error);
+      throw error;
     }
+
+    return this.connection;
   }
 }
 
-const db = Object.freeze(
-  new Database(
-    process.env.DB_HOST,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    process.env.DB_NAME,
-    process.env.DB_PASSWORD
-  ).connect()
-);
-
+const db = Database.getInstance().connect();
 module.exports = db;
